f0cab0f3ff32074ced2f12d4db10867825d768de
nagrapoonam <nagra.poonam1@gmail.com>
2023-07-13 21:17:44 -0400

    error and exceptional handling code added

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
-from flask import Flask, render_template, request, redirect, url_for
 from datamanager.json_data_manager import JSONDataManager
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-data_manager = JSONDataManager('data/movies.json')  # Use the appropriate path to your JSON file
+data_manager = JSONDataManager(
+    'data/movies.json')  # Use the appropriate path to your JSON file
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 @app.route('/users')
 def list_users():
     users = data_manager.get_all_users()
     return render_template('users.html', users=users)
 
+
 @app.route('/users/<int:user_id>')
 def list_movies(user_id):
     movies = data_manager.get_user_movies(user_id)
     return render_template('movies.html', movies=movies)
+
 
 @app.route('/add_user', methods=['GET', 'POST'])
 def add_user():
@@ -26,39 +31,60 @@ def add_user():
         return render_template('add_user.html', message=message)
     return render_template('add_user.html')
 
+
 @app.route('/users/<int:user_id>/add_movie', methods=['GET', 'POST'])
 def add_movie(user_id):
     if request.method == 'POST':
         movie_name = request.form['movie_name']
         message = data_manager.add_movie(user_id, movie_name)
-        return render_template('add_movie.html', user_id=user_id, message=message)
+        return render_template('add_movie.html', user_id=user_id,
+                               message=message)
     return render_template('add_movie.html', user_id=user_id)
 
 
-@app.route('/users/<int:user_id>/update_movie/<int:movie_id>', methods=['GET', 'POST'])
+@app.route('/users/<int:user_id>/update_movie/<int:movie_id>',
+           methods=['GET', 'POST'])
 def update_movie(user_id, movie_id):
     if request.method == 'POST':
         director = request.form.get('director')
         year = request.form.get('year')
         rating = request.form.get('rating')
 
-        result = data_manager.update_movie(user_id, movie_id, director, year, rating)
+        result = data_manager.update_movie(user_id, movie_id, director, year,
+                                           rating)
         return result
     else:
         # Retrieve movie information from the database or JSON file
         # Implement your logic here
-        movie = data_manager.get_movie(user_id, movie_id)  # Replace with your logic to retrieve movie info
+        movie = data_manager.get_movie(user_id,
+                                       movie_id)  # Replace with your logic to retrieve movie info
 
         if movie:
-            return render_template('update_movie.html', user_id=user_id, movie_id=movie_id, movie=movie)
+            return render_template('update_movie.html', user_id=user_id,
+                                   movie_id=movie_id, movie=movie)
         else:
-            movie_name = data_manager.get_movie_name(movie_id)  # Replace with your logic to retrieve movie name
-            user_name = data_manager.get_user_name(user_id)  # Replace with your logic to retrieve user name
+            movie_name = data_manager.get_movie_name(
+                movie_id)  # Replace with your logic to retrieve movie name
+            user_name = data_manager.get_user_name(
+                user_id)  # Replace with your logic to retrieve user name
             return f"Movie '{movie_name}' not found for user '{user_name}'."
+
+
 @app.route('/users/<int:user_id>/delete_movie/<int:movie_id>')
 def delete_movie(user_id, movie_id):
     message = data_manager.delete_movie(user_id, movie_id)
     return redirect(url_for('list_movies', user_id=user_id))
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
--- datamanager/json_data_manager.py
+++ datamanager/json_data_manager.py
@@ -1,159 +1,193 @@
 import json
+
 import requests
+
 from .data_manager_interface import DataManagerInterface
+
 
 class JSONDataManager(DataManagerInterface):
     def __init__(self, filename):
         self.filename = filename
 
     def get_all_users(self):
-        # Return a list of all users
-        with open(self.filename, "r") as json_file:
-            data = json.load(json_file)
-            users = []
-            for user_id, user_info in data.items():
-                user = {
-                    'id': user_id,
-                    'name': user_info['name'],
-                    'movies': user_info['movies']
-                }
-                users.append(user)
-            return users
+        try:
+            with open(self.filename, "r") as json_file:
+                data = json.load(json_file)
+                users = []
+                for user_id, user_info in data.items():
+                    user = {
+                        'id': user_id,
+                        'name': user_info['name'],
+                        'movies': user_info['movies']
+                    }
+                    users.append(user)
+                return users
+        except IOError as e:
+            # Handle the IOError exception (e.g., log the error, return an error message, etc.)
+            # ...
+            return []
 
     def get_user_movies(self, user_id):
-        # Return a list of all movies for a given user
-        with open(self.filename) as json_file:
-            data = json.load(json_file)
-            user_info = data.get(str(user_id))
-            if user_info:
-                user_movies = user_info.get("movies", {})
-                movies = []
-                for movie_id, movie_info in user_movies.items():
-                    movie = {
-                        'id': movie_id,
-                        'name': movie_info['name'],
-                        'director': movie_info['director'],
-                        'year': movie_info['year'],
-                        'rating': movie_info['rating']
-                    }
-                    movies.append(movie)
-                return movies
+        try:
+            with open(self.filename) as json_file:
+                data = json.load(json_file)
+                user_info = data.get(str(user_id))
+                if user_info:
+                    user_movies = user_info.get("movies", {})
+                    movies = []
+                    for movie_id, movie_info in user_movies.items():
+                        movie = {
+                            'id': movie_id,
+                            'name': movie_info['name'],
+                            'director': movie_info['director'],
+                            'year': movie_info['year'],
+                            'rating': movie_info['rating']
+                        }
+                        movies.append(movie)
+                    return movies
+        except IOError as e:
+            # Handle the IOError exception
+            # ...
+            return []
+
         return []
 
     def add_user(self, user_name):
-        # Add a new user
-        with open(self.filename, "r+") as json_file:
-            data = json.load(json_file)
-            user_ids = list(data.keys())
-            new_user_id = str(int(max(user_ids)) + 1) if user_ids else '1'
-            for user_info in data.values():
-                if user_info['name'] == user_name:
-                    return f"Username '{user_name}' already exists. Please choose a different username."
-            data[new_user_id] = {
-                'name': user_name,
-                'movies': {}
-            }
-            json_file.seek(0)
-            json.dump(data, json_file, indent=4)
-            json_file.truncate()
-            return f"User '{user_name}' added successfully."
-
-    def add_movie(self, user_id, movie_name):
-        # Add a movie for a particular user
-        api_url = f"http://www.omdbapi.com/?apikey=4bf81bd7&t={movie_name}"
-        response = requests.get(api_url)
-        movie_data = response.json()
-
-        if movie_data.get("Response") != "True":
-            return f"Movie '{movie_name}' not found in OMDB data."
-
-        movie = {
-            'name': movie_name,
-            'director': movie_data.get("Director"),
-            'year': movie_data.get("Year"),
-            'rating': movie_data.get("imdbRating")
-        }
-
-        user_id = str(user_id)
-        with open(self.filename, "r+") as json_file:
-            data = json.load(json_file)
-            user_info = data.get(user_id)
-            if user_info:
-                user_movies = user_info.setdefault('movies', {})
-                movie_id = str(len(user_movies) + 1)
-                user_movies[movie_id] = movie
-
+        try:
+            with open(self.filename, "r+") as json_file:
+                data = json.load(json_file)
+                user_ids = list(data.keys())
+                new_user_id = str(int(max(user_ids)) + 1) if user_ids else '1'
+                for user_info in data.values():
+                    if user_info['name'] == user_name:
+                        return f"Username '{user_name}' already exists. Please choose a different username."
+                data[new_user_id] = {
+                    'name': user_name,
+                    'movies': {}
+                }
                 json_file.seek(0)
                 json.dump(data, json_file, indent=4)
                 json_file.truncate()
+                return f"User '{user_name}' added successfully."
+        except IOError as e:
+            # Handle the IOError exception
+            # ...
+            return f"An error occurred while adding a user."
 
-                return f"Movie '{movie_name}' added for user {self.get_user_name(user_id)} successfully."
+    def add_movie(self, user_id, movie_name):
+        try:
+            api_url = f"http://www.omdbapi.com/?apikey=4bf81bd7&t={movie_name}"
+            response = requests.get(api_url)
+            movie_data = response.json()
 
-        return f"User with ID {user_id} not found."
-    def update_movie(self, user_id, movie_id, director, year, rating):
-        user_id = str(user_id)
-        movie_id = str(movie_id)
+            if movie_data.get("Response") != "True":
+                return f"Movie '{movie_name}' not found in OMDB data."
 
-        with open(self.filename, "r+") as json_file:
-            data = json.load(json_file)
-            user_info = data.get(user_id)
+            movie = {
+                'name': movie_name,
+                'director': movie_data.get("Director"),
+                'year': movie_data.get("Year"),
+                'rating': movie_data.get("imdbRating")
+            }
 
-            if user_info:
-                movie = user_info.get("movies", {}).get(movie_id)
-
-                if movie:
-                    movie["director"] = director
-                    movie["year"] = year
-                    movie["rating"] = rating
+            user_id = str(user_id)
+            with open(self.filename, "r+") as json_file:
+                data = json.load(json_file)
+                user_info = data.get(user_id)
+                if user_info:
+                    user_movies = user_info.setdefault('movies', {})
+                    movie_id = str(len(user_movies) + 1)
+                    user_movies[movie_id] = movie
 
                     json_file.seek(0)
                     json.dump(data, json_file, indent=4)
                     json_file.truncate()
-                    return f"Movie '{movie['name']}' updated for user '{user_info['name']}' successfully."
 
-                return f"Movie '{self.get_movie_name(movie_id)}' not found for user '{self.get_user_name(user_id)}'."
+                    return f"Movie '{movie_name}' added for user {self.get_user_name(user_id)} successfully."
 
-            return f"User with ID '{self.get_user_name(user_id)}' not found."
+            return f"User with ID {user_id} not found."
+        except requests.RequestException as e:
+            # Handle the RequestException (e.g., log the error, return an error message, etc.)
+            # ...
+            return f"An error occurred while adding the movie."
+
+    def update_movie(self, user_id, movie_id, director, year, rating):
+        try:
+            user_id = str(user_id)
+            movie_id = str(movie_id)
+
+            with open(self.filename, "r+") as json_file:
+                data = json.load(json_file)
+                user_info = data.get(user_id)
+
+                if user_info:
+                    movie = user_info.get("movies", {}).get(movie_id)
+
+                    if movie:
+                        movie["director"] = director
+                        movie["year"] = year
+                        movie["rating"] = rating
+
+                        json_file.seek(0)
+                        json.dump(data, json_file, indent=4)
+                        json_file.truncate()
+                        return f"Movie '{movie['name']}' updated for user '{user_info['name']}' successfully."
+
+                    return f"Movie '{self.get_movie_name(movie_id)}' not found for user '{self.get_user_name(user_id)}'."
+
+                return f"User with ID '{self.get_user_name(user_id)}' not found."
+        except IOError as e:
+            # Handle the IOError exception
+            # ...
+            return f"An error occurred while updating the movie."
 
     def delete_movie(self, user_id, movie_id):
-        user_id = str(user_id)
-        movie_id = str(movie_id)
+        try:
+            user_id = str(user_id)
+            movie_id = str(movie_id)
 
-        with open(self.filename, "r+") as json_file:
-            data = json.load(json_file)
-            user_info = data.get(user_id)
-            if user_info:
-                movies = user_info.get("movies", {})
-                movie = movies.get(movie_id)
-                if movie:
-                    del movies[movie_id]
-                    json_file.seek(0)
-                    json.dump(data, json_file, indent=4)
-                    json_file.truncate()
-                    return f"Movie '{movie['name']}' deleted for user with ID {user_id} successfully."
-                return f"Movie not found for user with ID {user_id}."
-            return f"User with ID {user_id} not found."
+            with open(self.filename, "r+") as json_file:
+                data = json.load(json_file)
+                user_info = data.get(user_id)
+                if user_info:
+                    movies = user_info.get("movies", {})
+                    movie = movies.get(movie_id)
+                    if movie:
+                        del movies[movie_id]
+                        json_file.seek(0)
+                        json.dump(data, json_file, indent=4)
+                        json_file.truncate()
+                        return f"Movie '{movie['name']}' deleted for user with ID {user_id} successfully."
+                    return f"Movie not found for user with ID {user_id}."
+                return f"User with ID {user_id} not found."
+        except IOError as e:
+            # Handle the IOError exception
+            # ...
+            return f"An error occurred while deleting the movie."
 
     def get_movie(self, user_id, movie_id):
-        # Get a specific movie for a given user by user ID and movie ID
-        user_id = str(user_id)
-        movie_id = str(movie_id)
+        try:
+            user_id = str(user_id)
+            movie_id = str(movie_id)
 
-        with open(self.filename, "r") as json_file:
-            data = json.load(json_file)
-            user_info = data.get(user_id)
-            if user_info:
-                movies = user_info.get("movies", {})
-                movie = movies.get(movie_id)
-                if movie:
-                    return {
-                        'id': movie_id,
-                        'name': movie['name'],
-                        'director': movie['director'],
-                        'year': movie['year'],
-                        'rating': movie['rating']
-                    }
-        return None
+            with open(self.filename, "r") as json_file:
+                data = json.load(json_file)
+                user_info = data.get(user_id)
+                if user_info:
+                    movies = user_info.get("movies", {})
+                    movie = movies.get(movie_id)
+                    if movie:
+                        return {
+                            'id': movie_id,
+                            'name': movie['name'],
+                            'director': movie['director'],
+                            'year': movie['year'],
+                            'rating': movie['rating']
+                        }
+        except IOError as e:
+            # Handle the IOError exception
+            # ...
+            return None
 
     def get_movie_name(self, movie_id):
         # Replace with your logic to retrieve movie name based on movie ID
