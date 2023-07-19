a117d4910fd60915c017deef8d405defe377f00c
nagrapoonam <nagra.poonam1@gmail.com>
2023-07-18 21:20:22 -0400

    updating html and css

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, get_flashed_messages
+from flask import Flask, render_template, request, redirect, url_for, flash, get_flashed_messages, session
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 from datamanager.json_data_manager import JSONDataManager
 import requests
@@ -20,6 +20,8 @@ class User(UserMixin):
         self.username = username
         self.password = password
 
+    def get_id(self):
+        return str(self.id)
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -33,7 +35,9 @@ def load_user(user_id):
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    users = data_manager.get_all_users()
+    return render_template('index.html', users=users)
+    # return render_template('index.html', users=users, url_for_login=lambda user_id: url_for('login', user_id=user_id))
 
 
 
@@ -46,15 +50,15 @@ def register():
         message = data_manager.add_user(username, password)
 
         if message.startswith('User'):
-            flash('Registration successful. Please log in.')
+            flash('Registration successful. Please log in.', 'success')
             return redirect(url_for('login'))
-        else:
-            flash(message, 'error')
+
+        flash(message, 'error')
 
     return render_template('register.html')
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login/', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('list_movies', user_id=current_user.id))
@@ -74,8 +78,6 @@ def login():
 
     return render_template('login.html')
 
-
-
 @app.route('/logout')
 @login_required
 def logout():
@@ -84,18 +86,17 @@ def logout():
     return redirect(url_for('home'))
 
 
-@app.route('/users')
-@login_required
-def list_users():
-    users = data_manager.get_all_users()
-    return render_template('users.html', users=users)
-
-
 @app.route('/users/<int:user_id>/list_movies')
 @login_required
 def list_movies(user_id):
+    if current_user.id != str(user_id):
+        flash("You do not have permission to access this user's movies.")
+        flash("Logout and login back to access this user's movies.")
+        return redirect(url_for('home'))
+
     user = data_manager.get_user(user_id)
     flash_message = request.args.get('flash_message')
+
     if user:
         movies = user.get('movies', {})
         return render_template('list_movies.html', movies=movies, user_id=user_id, flash_message=flash_message)
@@ -103,17 +104,16 @@ def list_movies(user_id):
         flash('User not found.')
         return redirect(url_for('home'))
 
-
 @app.route('/users/<int:user_id>/add_movie', methods=['GET', 'POST'])
 @login_required
 def add_movie(user_id):
     if request.method == 'POST':
         movie_name = request.form['movie_name']
         message = data_manager.add_movie(user_id, movie_name)
-        return redirect(url_for('list_movies', user_id=user_id, flash_message=message))
+        flash(message)  # Add flash message here
+        return redirect(url_for('list_movies', user_id=user_id))
 
     return render_template('add_movie.html', user_id=user_id)
-
 
 @app.route('/users/<int:user_id>/update_movie/<int:movie_id>', methods=['GET', 'POST'])
 @login_required
@@ -124,7 +124,8 @@ def update_movie(user_id, movie_id):
         rating = request.form.get('rating')
 
         result = data_manager.update_movie(user_id, movie_id, director, year, rating)
-        return redirect(url_for('list_movies', user_id=user_id, flash_message=result))
+        flash(result)  # Add flash message here
+        return redirect(url_for('list_movies', user_id=user_id))
     else:
         movie = data_manager.get_movie(user_id, movie_id)
         if movie:
@@ -137,7 +138,8 @@ def update_movie(user_id, movie_id):
 @login_required
 def delete_movie(user_id, movie_id):
     message = data_manager.delete_movie(user_id, movie_id)
-    return redirect(url_for('list_movies', user_id=user_id, flash_message=message))
+    flash(message)  # Add flash message here
+    return redirect(url_for('list_movies', user_id=user_id))
 
 
 
--- static/script.js
+++ static/script.js
@@ -1,82 +1,29 @@
-document.addEventListener("DOMContentLoaded", function() {
-  // Fetch random movies from the OMDB API
-  fetchRandomMovies()
-    .then(function(movies) {
-      // Generate carousel slides
-      generateCarouselSlides(movies);
+// Fetch movie data from movies.json
+fetch('movies.json')
+  .then(response => response.json())
+  .then(data => {
+    const movies = data;
+    const header = document.querySelector('header');
 
-      // Generate random movies
-      generateRandomMovies(movies);
-    })
-    .catch(function(error) {
-      console.error("Error:", error);
-    });
-});
+    // Iterate over each movie
+    for (const userId in movies) {
+      const user = movies[userId];
+      const userMovies = user.movies;
 
-function fetchRandomMovies() {
-  const api_url = "http://www.omdbapi.com/?apikey=4bf81bd7&t";
-  const movieTitles = ["Silo", "The Witcher", "Sound Of Freedom", "Black Mirror", "Titanic", "The Flash", "Extraction II", "Yellowstone", "The Boys", "The Covenant", "Barbie"];
+      // Iterate over each movie poster
+      for (const movieId in userMovies) {
+        const movie = userMovies[movieId];
+        const posterUrl = movie.poster;
 
-  // Fetch movie details from the OMDB API for each title
-  const fetchMoviePromises = movieTitles.map((title) => {
-    const url = `${api_url}=${encodeURIComponent(title)}`;
-    return fetch(url)
-      .then((response) => response.json())
-      .then((data) => ({
-        title: data.Title,
-        poster: data.Poster,
-        director: data.Director,
-        year: data.Year,
-        rating: data.imdbRating
-      }))
-      .catch((error) => {
-        console.error(`Error fetching movie "${title}":`, error);
-        return null;
-      });
+        // Create an image element for the movie poster
+        const posterImage = document.createElement('img');
+        posterImage.src = posterUrl;
+
+        // Append the poster image to the header
+        header.appendChild(posterImage);
+      }
+    }
+  })
+  .catch(error => {
+    console.error('Error fetching movie data:', error);
   });
-
-  // Return a promise that resolves to an array of movies
-  return Promise.all(fetchMoviePromises).then((movies) => movies.filter(Boolean));
-}
-
-function generateCarouselSlides(movies) {
-  const carousel = document.querySelector(".carousel");
-
-  // Generate carousel slides dynamically
-  movies.forEach(function(movie) {
-    const slide = document.createElement("div");
-    slide.classList.add("carousel-slide");
-
-    const img = document.createElement("img");
-    img.src = movie.poster;
-    img.alt = movie.title;
-
-    slide.appendChild(img);
-    carousel.appendChild(slide);
-  });
-
-  // Initialize the carousel using Bootstrap Carousel
-  $(".carousel").carousel();
-}
-
-function generateRandomMovies(movies) {
-  const movieGrid = document.querySelector(".movie-grid");
-
-  // Generate random movies dynamically
-  movies.slice(0, 10).forEach(function(movie) {
-    const movieElement = document.createElement("div");
-    movieElement.classList.add("movie");
-
-    const img = document.createElement("img");
-    img.src = movie.poster;
-    img.alt = movie.title;
-
-    const title = document.createElement("h3");
-    title.classList.add("movie-title");
-    title.textContent = movie.title;
-
-    movieElement.appendChild(img);
-    movieElement.appendChild(title);
-    movieGrid.appendChild(movieElement);
-  });
-}
